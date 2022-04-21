daaf057152c79d234f495363fb22053a08cfa374
mattbcoding <mattbodden@googlemail.com>
2022-04-21 18:24:47 +0100

    Add allowed hosts to settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ci-walkthrough-boutique-ado.herokuapp.com', 'http://127.0.0.1:8000/']
 
 
 # Application definition
